f25df7c884e81ef38187aeb6b1e31ffde8635171
Ivan Bogatyrev <yanagiori@gmail.com>
2021-01-22 05:39:28 +0300

    list stack done

--- lab_04/stack_list.c
+++ lab_04/stack_list.c
@@ -4,25 +4,62 @@
 
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "stack_list.h"
 
-int push_list(list_stack_t **head, int value)
+int push_list(stack_list_t **head)
 {
-    list_stack_t *tmp = malloc(sizeof(list_stack_t));
+    stack_list_t *tmp = malloc(sizeof(stack_list_t));
     if (tmp == NULL)
     {
         return STACK_OVERFLOW;
     }
     tmp->next = *head;
-    tmp->value = value;
+    tmp->this = tmp;
     *head = tmp;
     return 0;
 }
 
-
-list_stack_t *pop_list(list_stack_t **head)
+int add_to_freed(stack_list_t **head, stack_list_t *new_item)
 {
-    list_stack_t *out;
+    stack_list_t *tmp = malloc(sizeof(stack_list_t));
+    if (tmp == NULL)
+    {
+        return STACK_OVERFLOW;
+    }
+    tmp->next = *head;
+    tmp->this = new_item->this;
+    *head = tmp;
+
+    return 0;
+}
+
+void find_in_freed(stack_list_t **head, stack_list_t *new_node_p)
+{
+    stack_list_t *temp = *head;
+    if (temp && temp->this == new_node_p->this)
+    {
+        *head = temp->next;
+        stack_list_t *tmp = temp;
+        temp = temp->next;
+        free(tmp);
+    }
+    while (temp && temp->next)
+    {
+        if (new_node_p->this == temp->next->this)
+        {
+            stack_list_t *tmp = temp->next->next;
+            free(temp->next);
+            temp->next = tmp;
+            return;
+        }
+        temp = temp->next;
+    }
+}
+
+stack_list_t *pop_list(stack_list_t **head)
+{
+    stack_list_t *out;
     if ((*head) == NULL)
     {
         exit(STACK_UNDERFLOW);
@@ -33,7 +70,7 @@ list_stack_t *pop_list(list_stack_t **head)
 }
 
 
-size_t get_size_list(const list_stack_t *head)
+size_t get_size_list(const stack_list_t *head)
 {
     size_t size = 0;
     while (head)
@@ -42,4 +79,41 @@ size_t get_size_list(const list_stack_t *head)
         head = head->next;
     }
     return size;
+}
+
+void print_stack_list(stack_list_t *head)
+{
+    stack_list_t *temp = head;
+    bool flag = false;
+    while (temp)
+    {
+        flag = true;
+        printf("0x%p\n", temp->this);
+        temp = temp->next;
+    }
+
+    if (!flag)
+    {
+        printf("The list is empty\n");
+    }
+}
+
+void get_current_state_list(stack_list_t *stack, stack_list_t *freedAddresses)
+{
+    printf("Current state of stack: \n");
+    print_stack_list(stack);
+
+    printf("\nList of freed addresses: \n");
+    print_stack_list(freedAddresses);
+}
+
+
+void free_list(stack_list_t *head)
+{
+    while (head)
+    {
+        stack_list_t *next = head->next;
+        free(head);
+        head = next;
+    }
 }
--- lab_04/stack_list.h
+++ lab_04/stack_list.h
@@ -10,10 +10,21 @@
 
 typedef struct node
 {
-    int value;
+    struct node *this;
     struct node *next;
-} list_stack_t;
+} stack_list_t;
 
 
+int push_list(stack_list_t **head);
+
+stack_list_t *pop_list(stack_list_t **head);
+
+void free_list(stack_list_t *head);
+
+int add_to_freed(stack_list_t **head, stack_list_t *new_item);
+
+void find_in_freed(stack_list_t **head, stack_list_t *new_node_p);
+
+void get_current_state_list(stack_list_t *stack, stack_list_t *freedAddresses);
 
 #endif //LAB_04_STACK_LIST_H
