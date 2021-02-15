a13c98e80c14a6cccb75c6d54da2b6df34216404
Ivan Bogatyrev <yanagiori@gmail.com>
2021-02-15 23:11:28 +0300

    dijkstra

--- lab_07/src/graph.c
+++ lab_07/src/graph.c
@@ -20,3 +20,50 @@ node_t *initNode()
     return node;
 }
 
+void printArray(node_t **array, int num)
+{
+    printf("\n");
+    for (int i = 0; i < num; i++)
+    {
+        printf("%d %d\n", array[i]->visited, array[i]->dist);
+    }
+}
+
+int dijkstra(int **table, node_t **array, int num)
+{
+    int temp;
+    int min_index = INT_MAX;
+    int min;
+    do
+    {
+        min_index = INT_MAX;
+        min = INT_MAX;
+
+        for (int i = 0; i < num; i++)
+        {
+            if ((array[i]->visited == false) && (array[i]->dist < min))
+            {
+                min = array[i]->dist;
+                min_index = i;
+            }
+        }
+
+        if (min_index != INT_MAX)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                if (table[min_index][i] > 0)
+                {
+                    temp = min + table[min_index][i];
+                    if (temp < array[i]->dist)
+                    {
+                        array[i]->dist = temp;
+                    }
+                }
+            }
+            array[min_index]->visited = true;
+        }
+    } while (min_index < INT_MAX);
+
+    return 0;
+}
